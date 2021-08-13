bed522b1e95b307a8941896edf40402c1bdd48c4
Jodis Fields <jodis.fields1@gmail.com>
2021-08-13 19:06:07 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Docker Install Script
+    ```
+    git clone https://github.com/jodisfields/Spudbrook.git
+    cd Spudbrook
+    sh setup.sh
+    ```
+# Packages Installed
+    apt-transport-https
+    ca-certificates
+    curl
+    gnupg
+    lsb-release
+    docker
+    docker-compose
+# Deployments
+    Wiki.JS
---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker $USER
+newgrp docker
