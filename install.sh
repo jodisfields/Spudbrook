0c8e79cce0121941611e91c250fdcc01228ccc45
Jodis Fields <jodis.fields1@gmail.com>
2021-08-14 20:56:22 +1000

    Workaround for newgrp command

--- install.sh
+++ install.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
-# Install Docker Dependencies
-echo "Installing Dependencies"
-sleep 3
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-# Fetch Official Docker Installation Script
-curl -fsSL https://get.docker.com -o get-docker.sh
-# Install Docker
-sudo sh get-docker.sh
-# Fetch Official Docker-Compose Installation Script
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# Enables Execution of Script
-sudo chmod +x /usr/local/bin/docker-compose
-# Adds User To "Docker" Group
-sudo usermod -aG docker $USER
-newgrp docker
-echo "Install Successful"
-sleep 3
-docker --version
-sleep 3
-docker-compose --version
-sleep 3
-cd docker-compose
-# initialize docker swarm
-docker swarm init
-# deploys portainer
-curl -L https://downloads.portainer.io/portainer-agent-stack.yml -o portainer-agent-stack.yml
-docker stack deploy -c portainer-agent-stack.yml portainer
+    echo "Installing Dependencies"
+    sleep 2
+    sudo apt-get install \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo usermod -aG docker $USER
+    newgrp docker << END
+    echo "Install Successful"
+    sleep 2
+    docker --version
+    sleep 2
+    docker-compose --version
+    sleep 2
+    cd docker-compose
+    docker swarm init
+    curl -L https://downloads.portainer.io/portainer-agent-stack.yml -o portainer-agent-stack.yml
+    docker stack deploy -c portainer-agent-stack.yml portainer
+    END
