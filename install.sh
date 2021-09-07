d3c87b6154d871e23c65e81f3f445625bc8d51eb
Jodis Fields <46502355+jodisfields@users.noreply.github.com>
2021-09-07 16:04:27 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,6 +23,4 @@
     docker swarm init
     curl -L https://downloads.portainer.io/portainer-agent-stack.yml -o portainer-agent-stack.yml
     docker stack deploy -c portainer-agent-stack.yml portainer
-    EOF
-    exit
-    exit
+    exit 0
